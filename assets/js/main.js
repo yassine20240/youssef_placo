a8d8037986a4560e4db1cb6097ba52f1984455d6
yassine khalis <yassinekhalis@gmail.com>
2026-01-20 13:16:47 +0100

    ??

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: BizLand
-* Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
-* Updated: Dec 05 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 
 (function() {
   "use strict";
@@ -104,6 +97,10 @@
   }
   window.addEventListener('load', aosInit);
 
+
+
+
+  
   /**
    * Initiate glightbox
    */
